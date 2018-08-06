8d9d3aab22a2305361a4e4c4a8f9153b64b9b59f
Egahi <okworisamuel@ymail.com>
2018-08-06 11:30:06 +0100

    create Operators.java

---
+++ Operators.java
@@ -0,0 +1,22 @@
+public class Operators {
+    public static void main(String[] args) {
+        float capital = 14000;
+        System.out.println("Capital:\t\t$" + capital);
+
+        // increase capital by 40%
+        capital += capital * 0.4;
+        System.out.println("Increased Capital:\t$" + capital);
+
+        // loss of $1500
+        capital -= 1500;
+        System.out.println("Capital after loss:\t$" + capital);
+
+        // increase of 12%
+        capital += capital * 0.12;
+        System.out.println("Increased Capital:\t$" + capital);
+
+        int number1 = 5, number2 = 6;
+        System.out.println("\n\n" + number1 + " / " + number2 + "\t\t\t\t= " + number1 / number2);
+        System.out.println("The Remainder from " + number1 + " / " + number2 + "\t= " + number1 % number2);   
+    }
+}
