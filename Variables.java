18d25444cc4240da39525c6602605d0beca8349f
Egahi <okworisamuel@ymail.com>
2018-08-06 10:42:06 +0100

    "Understanding variables in java"

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        final char UP = 'U';
+        byte initialLevel = 12;
+        short location = 13250;
+        int score = 3500100;
+        boolean newGame = true;
+
+        System.out.println("Level: " + initialLevel);
+        System.out.println("Up: " + UP);
+    }
+}
