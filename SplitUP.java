b9119e90fb4603e7efc02afa582e7b70c0b760f6
Egahi <okworisamuel@ymail.com>
2018-08-07 21:33:09 +0100

    modify SplitUP.java

--- SplitUP.java
+++ SplitUP.java
@@ -1,1 +1,13 @@
 import java.util.StringTokenizer;
+
+class SplitUP {
+    public static void main(String[] args) {
+        String birthday = "12/25/2018";
+
+        StringTokenizer string = new StringTokenizer(birthday, "/");
+
+        System.out.println("\n\nMonth: " + string.nextToken());
+        System.out.println("Day: "  + string.nextToken());
+        System.out.println("Year: " + string.nextToken());
+    }
+}
