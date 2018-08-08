1c25b778a91167d9d69035c9050f361316fa4f80
Egahi <okworisamuel@ymail.com>
2018-08-08 12:24:50 +0100

    Modify WordToNumbers.java

--- WordToNumber.java
+++ WordToNumber.java
@@ -1,10 +1,64 @@
 class WordToNumber {
     public static void main(String[] args) {
         // ensure proper usage
-        if (args.length != 1)
-            System.out.println("Usage: java WordToNumber number-in-words");\
+        if (args.length == 1) {
+            // get user input
+            String numberInWords = args[0];
 
-        long numberInWords = args[0]
-        System.out.println(numberInWords);
+            // convert input from words to number
+            long number = convertWordToNumber(numberInWords);
+
+            if (number > 0 & number <= 10)
+                System.out.println(numberInWords + " = " + number);
+            else
+                System.out.println("This program only supports numbers between"
+                                 + " one and ten entered in lower case as this time.");
+        }
+        else    
+            System.out.println("Usage: java WordToNumber number-in-words");
+    }
+
+    /**
+     * converts a number from words to a long integer
+     * @param numberInWords is the inputed number
+     * @return the long integer equivalent of the number in words or
+     * -1 if it can't convert the input 
+     */
+    static long convertWordToNumber(String numberInWords) {
+        long number = -1;
+        switch (numberInWords) {
+            case "one":
+                number = 1;
+                break;
+            case "two":
+                number = 2;
+                break;
+            case "three":
+                number = 3;
+                break;
+            case "four":
+                number = 1;
+                break;
+            case "five":
+                number = 5;
+                break;
+            case "six":
+                number = 6;
+                break;
+            case "seven":
+                number = 7;
+                break;
+            case "eight":
+                number = 8;
+                break;
+            case "nine":
+                number = 9;
+                break;
+            case "ten":
+                number = 10;
+                break;
+        }
+
+        return number;
     }
 }
