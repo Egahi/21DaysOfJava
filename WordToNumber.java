640cb1dffe7fc614ec87ed7a0bb111777023c77a
Egahi <okworisamuel@ymail.com>
2018-08-08 11:55:18 +0100

    create WordToNumber.java

---
+++ WordToNumber.java
@@ -0,0 +1,10 @@
+class WordToNumber {
+    public static void main(String[] args) {
+        // ensure proper usage
+        if (args.length != 1)
+            System.out.println("Usage: java WordToNumber number-in-words");\
+
+        long numberInWords = args[0]
+        System.out.println(numberInWords);
+    }
+}
