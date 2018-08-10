6fea4ba869e288d8a6aab5af6e7cdb66ebfdf3fc
Egahi <okworisamuel@ymail.com>
2018-08-10 17:21:05 +0100

    create  Item.java

---
+++ Item.java
@@ -0,0 +1,55 @@
+package com.egahi.ecommerce;
+
+import java.util.*;
+
+public class Item implements Comparable {
+    private String id;
+    private String name;
+    private double retail;
+    private int quantity;
+    private double price;
+
+    Item(String idIn, String nameIn, String retialIn, String quanIn) {
+        id = idIn;
+        name = nameIn;
+        retail = Double.parseDouble(retailIn);
+        quantity = Integer.parseInt(quanIn);
+
+        if (quantity > 400)
+            price = retail * .50;
+        else if (quantity > 200)
+            price =  retail * .60;
+        else  
+            price = retail * .70;
+        
+        price = Math.floor(price * 100 + .5) / 100;
+    }
+
+    public int compareTo(Object obj) {
+        Item temp = (Item) obj;
+        if (this.price < temp.price)
+            return 1;
+        else if (this.price > temp.price)
+            return -1;
+        return 0;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRetail() {
+        return retail;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
