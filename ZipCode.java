01f527b2400b50992dbc5fc7f91c7007053ad2d4
Egahi <okworisamuel@ymail.com>
2018-08-10 19:10:04 +0100

    add comments

--- ZipCode.java
+++ ZipCode.java
@@ -1,9 +1,16 @@
 class ZipCode {
     private int zipCode;
 
+    /**
+     * sets the zip code value
+     * accepts only 5 digits or 9 digits number
+     * @param zipCode is the value to be asigned as the zip code
+     * @return true if code is valid else false
+     */
     public Boolean setZipCode(int zipCode) {
         Boolean valid = true;
 
+        // only 5 or 9 digits numbers are valid
         if ((zipCode >= 10000 & zipCode <= 99999) | 
             (zipCode >= 100000000 & zipCode <= 999999999))
             this.zipCode = zipCode;
@@ -20,8 +27,10 @@ class ZipCode {
     public static void main(String[] arguments) {
         ZipCode customZipCode = new ZipCode();
 
+        // enusure proper usage
         if (arguments.length == 1) {
             Boolean valid = customZipCode.setZipCode(Integer.parseInt(arguments[0]));
+            
             if (valid)
                 System.out.println("Zip Code: " + customZipCode.getZipCode());
             else
