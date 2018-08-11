d16dc552d2c2b499772eda443aa0f57804cea16c
Egahi <okworisamuel@ymail.com>
2018-08-11 12:35:18 +0100

    Modify MembebBioData.java

--- MemberBioData.java
+++ MemberBioData.java
@@ -9,20 +9,24 @@ public class MemberBioData extends JWindow {
         setSize(300, 300);
         setBounds(100, 100, 600, 400);
         getContentPane().setBackground(Color.BLACK);
-
+        setVisible(true);
+        // create buttons
         JButton saveButton = new JButton("Save");
         JButton openButton = new JButton("Open");
         JButton closeButton = new JButton("Close");
 
+        // create  panel for buttons
         JPanel buttonPanel = new  JPanel();
+
+        // add  buttons to panel
         buttonPanel.add(saveButton);
         buttonPanel.add(openButton);
         buttonPanel.add(closeButton);
 
-        add(buttonPanel);
+        buttonPanel.setBackground(Color.BLUE);
 
-        setVisible(true);
-        
+        // add panel to window
+        add(buttonPanel);
     }
 
     public static void main(String[] args) {
