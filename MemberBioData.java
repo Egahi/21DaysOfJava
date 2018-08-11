00ed1e57785b612dd5482b9831db734c05633189
Egahi <okworisamuel@ymail.com>
2018-08-11 12:23:14 +0100

    change frame to window and add save, open and close  buttons

--- MemberBioData.java
+++ MemberBioData.java
@@ -1,13 +1,28 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class MemberBioData extends JFrame {
+public class MemberBioData extends JWindow {
     public MemberBioData() {
-        setTitle("Member Bio Data");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super();
+        // setTitle("Member Bio Data");
+        // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
-        setVisible(true);
+        setBounds(100, 100, 600, 400);
         getContentPane().setBackground(Color.BLACK);
+
+        JButton saveButton = new JButton("Save");
+        JButton openButton = new JButton("Open");
+        JButton closeButton = new JButton("Close");
+
+        JPanel buttonPanel = new  JPanel();
+        buttonPanel.add(saveButton);
+        buttonPanel.add(openButton);
+        buttonPanel.add(closeButton);
+
+        add(buttonPanel);
+
+        setVisible(true);
+        
     }
 
     public static void main(String[] args) {
