db1ea5c16c3ca33275d88a8540ae6195863ed1ad
Egahi <okworisamuel@ymail.com>
2018-08-11 13:15:15 +0100

    add action to close button

--- MemberBioData.java
+++ MemberBioData.java
@@ -1,19 +1,22 @@
 import java.awt.*;
 import javax.swing.*;
+import  java.awt.event.*;
 
-public class MemberBioData extends JWindow {
+public class MemberBioData extends JWindow implements ActionListener {
+    JButton closeButton;
+
     public MemberBioData() {
         super();
         // setTitle("Member Bio Data");
         // setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
         setBounds(100, 100, 600, 400);
-        getContentPane().setBackground(Color.BLACK);
     
         // create buttons
         JButton saveButton = new JButton("Save");
         JButton openButton = new JButton("Open");
-        JButton closeButton = new JButton("Close");
+        closeButton = new JButton("Close");
+        closeButton.addActionListener(this);
 
         // create  panel for buttons
         JPanel buttonPanel = new  JPanel();
@@ -29,6 +32,15 @@ public class MemberBioData extends JWindow {
         add(buttonPanel);
 
         setVisible(true);
+        getContentPane().setBackground(Color.BLACK);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        Object source = event.getSource();
+
+        if (source == closeButton) {
+            System.exit(0);
+        }
     }
 
     public static void main(String[] args) {
