e2459d10ddec39f008c2e1cd2c0185c044401948
Egahi <okworisamuel@ymail.com>
2018-08-15 18:27:28 +0100

    modify Authenticator.java

--- Authenticator.java
+++ Authenticator.java
@@ -17,6 +17,7 @@ public class Authenticator extends javax.swing.JFrame {
         JLabel passwordLabel = new JLabel("Password: ");
         JLabel commentsLabel = new JLabel("Comments: ");
         comments.setLineWrap(true);
+        password.setEchoChar('*');
         panel.add(usernameLabel);
         panel.add(username);
         panel.add(passwordLabel);
