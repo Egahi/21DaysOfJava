938022db9d7a738da4e3ea9f8e419fa7fa9eac69
Egahi <okworisamuel@ymail.com>
2018-08-15 17:29:28 +0100

    create SimpleWindow.java

---
+++ SimpleWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.JWindow;
+
+public class SimpleWindow {
+    public SimpleWindow() {
+        super();
+        setBounds(400, 300, 10, 10);
+        setVisible(true);
+    }
+
+    public static void main(String[] arguments) {
+        SimpleWindow sw = new SimpleWindow();
+        for (int i = 10;  i < 400; i++) {
+            sw.setBounds(400 - (i/2), 300 - (i/2), i, i);
+        }
+    }
+}
