6ec491dc68ccfe3cb2f3a42b2caa10d428754859
Egahi <okworisamuel@ymail.com>
2018-08-15 17:34:44 +0100

    modify SimpleWindow  to extend JWindow

--- SimpleWindow.java
+++ SimpleWindow.java
@@ -1,15 +1,15 @@
 import javax.swing.JWindow;
 
-public class SimpleWindow {
+public class SimpleWindow extends JWindow {
     public SimpleWindow() {
-        super();
-        setBounds(400, 300, 10, 10);
-        setVisible(true);
+    super();
+    setBounds(400, 300, 10, 10);
+    setVisible(true);
     }
 
     public static void main(String[] arguments) {
-        SimpleWindow sw = new SimpleWindow();
-        for (int i = 10;  i < 400; i++) {
+    SimpleWindow sw = new SimpleWindow();
+        for (int i = 10; i < 400; i++) {
             sw.setBounds(400 - (i/2), 300 - (i/2), i, i);
         }
     }
