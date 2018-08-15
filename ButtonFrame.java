e121799a454dc0019c90aa034c1ccddfe9fe6b6e
Egahi <okworisamuel@ymail.com>
2018-08-15 17:57:01 +0100

    Add photos button with an image icon

--- ButtonFrame.java
+++ ButtonFrame.java
@@ -4,16 +4,20 @@ public class ButtonFrame  extends JFrame {
     JButton load = new JButton("Load");
     JButton save = new JButton("Save");
     JButton unsubscribe = new JButton("Unsubscribe");
+    ImageIcon photosIcon = new ImageIcon("sunset_over_ocean.jpg");
+    JButton photos = new JButton("Photos", photosIcon);
 
     public ButtonFrame() {
         super("Button Frame");
-        setSize(140, 170);
+        // setSize(140, 170);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel pane = new JPanel();
-        pane.add(load);
-        pane.add(save);
-        pane.add(unsubscribe);
-        add(pane);
+        JPanel panel = new JPanel();
+        panel.add(load);
+        panel.add(save);
+        panel.add(unsubscribe);
+        panel.add(photos);
+        add(panel);
+        pack();
         setVisible(true);
     }
 
