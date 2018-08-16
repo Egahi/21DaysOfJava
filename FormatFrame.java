82258946dbf475c093638d2f51f2e31c5b9b96f5
Egahi <okworisamuel@ymail.com>
2018-08-16 10:16:29 +0100

    add drop downlist to FormatFrame

--- FormatFrame.java
+++ FormatFrame.java
@@ -2,24 +2,38 @@ import javax.swing.*;
 
 public class FormatFrame extends JFrame {
     JRadioButton[] teams = new JRadioButton[4];
+    String[] formats = {"Atom", "RSS 0.92", "RSS 1.0", "RSS 2.0"};
+    JComboBox formatBox = new JComboBox();
 
     public FormatFrame() {
         super("Choose an Output Format");
         setSize(320, 120);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        teams[0] = new JRadioButton("Atom");
-        teams[1] = new JRadioButton("RSS 0.92");
-        teams[2] = new JRadioButton("RSS 1.0");
-        teams[3] = new JRadioButton("RSS 2.0", true);
+        
+        // create radio buttons
+        // create drop down list items
+        for (int i = 0, j = formats.length; i < j; i++) {
+            teams[i] = new JRadioButton(formats[i]);
+            formatBox.addItem(formats[i]);
+        }
+        
         JPanel panel = new JPanel();
         JLabel chooseLabel = new JLabel(
             "Choose an Output format for syndicated  news items.");
         panel.add(chooseLabel);
         ButtonGroup group = new ButtonGroup();
+
+        // add radio buttons in a group
+        // add buttons to the panel
         for (int i = 0, j = teams.length; i < j; i++) {
             group.add(teams[i]);
             panel.add(teams[i]);
         }
+
+
+        JLabel formatLabel = new JLabel("Output formats");
+        panel.add(formatLabel);
+        panel.add(formatBox);
         add(panel);
         setVisible(true);
     }
