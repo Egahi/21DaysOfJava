7b56d557bc00a31906558c950cf0d5923790b97d
egahi <okworisamuel6@gmail.com>
2018-08-24 12:52:42 +0100

    modify class

--- BrickBreaker/MainFrame.java
+++ BrickBreaker/MainFrame.java
@@ -2,10 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
+    ImageIcon backgroundImage;
     public MainFrame() {
         super("Break Breaker");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(250, 250, 250, 250);
+
+        JPanel pane = new JPanel();
+        pane.setBackground(Color.black);
+        add(pane);
         setVisible(true);
     }
  
